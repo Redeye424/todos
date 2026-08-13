bf4a657a557ea0749f501837cc2d98ad7e02133c
Redeye424 <shawngoldberg424@gmail.com>
2026-08-12 20:57:29 -0400

    afaf

--- todos/settings.py
+++ todos/settings.py
@@ -96,22 +96,16 @@ WSGI_APPLICATION = 'todos.wsgi.application'
 #}
 
 
-if os.environ.get("DATABASE_URL"):
-    import dj_database_url
-
-    DATABASES = {
-        "default": dj_database_url.config(
-            conn_max_age=600,
-            conn_health_checks=True,
-        )
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
     }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
